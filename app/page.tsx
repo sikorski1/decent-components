350c0ad694bae726091c8cd7c457cfc447a7c6e1
sikorski1 <kacpersikora.praca@gmail.com>
2026-03-02 23:50:05 +0100

    feat: add btn 2

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,30 @@
-import "./page.css"
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import "./gradient-button.css";
+import "./btn.css";
 export default function Page() {
 	return (
 		<div className="p-10">
-			<Card className="min-h-100">
+			<Card>
 				<CardHeader>
 					<CardTitle className="font-rotonto text-6xl text-center">BUTTONS</CardTitle>
 				</CardHeader>
-				<CardContent>
-					<button className="btn-fancy">
-                        <span className="glass"></span>
-						<span className="text">+ Add to cart</span>
-					</button>
+				<CardContent className="grid grid-cols-4 gap-4 grid-rows-[repeat(2,minmax(150px,auto))]">
+					<div className="flex items-center justify-center bg-gray-100 rounded-xl aspect-square">
+						<button className="btn-fancy max-w-50">
+							<span className="glass"></span>
+							<span className="text">+ Add to cart</span>
+						</button>
+					</div>
+					<div className="flex items-center justify-center bg-gray-100 rounded-xl aspect-square">
+						<button className="btn-1 max-w-50">
+							<span className="text">+ Add to cart</span>
+						</button>
+					</div>
+					<div className="flex items-center justify-center bg-gray-100 rounded-xl aspect-square">
+						<button className="btn-1-clip-path max-w-50">
+							<span className="text">+ Add to cart</span>
+						</button>
+					</div>
 				</CardContent>
 			</Card>
 		</div>
